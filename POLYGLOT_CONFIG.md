a75f2e6d62e68c78174cc90c9ba2c637af43241f
BoilerGuy01 <20452272+BoilerGuy01@users.noreply.github.com>
2020-08-06 21:06:44 -0700

    Initial commit Have basic statuses all working Changed to make discover manual

---
+++ POLYGLOT_CONFIG.md
@@ -0,0 +1,1 @@
+# Update 'MQTT_HOST' to match the MQTT_HOST for your TeslaMate MQTT broker
---
+++ teslamate-poly.py
@@ -0,0 +1,341 @@
+#!/usr/bin/env python
+try:
+    import polyinterface,logging
+    import paho.mqtt.client as mqtt
+except ImportError:
+    import pgc_interface as polyinterface
+import sys
+from urllib.request import urlopen
+import xml.etree.ElementTree as ET
+import time
+import json
+import requests
+
+LOGGER = polyinterface.LOGGER
+LOGGER.setLevel(logging.WARNING)
+LOGGER.setLevel(logging.INFO)
+LOGGER.setLevel(logging.DEBUG)
+_PARM_MQTT_HOST_NAME = "MQTT_HOST"
+MILES_PER_KM=0.621371
+
+class Controller(polyinterface.Controller):
+    def controller_on_connect(self, client, userdata, flags, rc):
+      LOGGER.debug('Connected with result code '.format(str(rc)))
+      client.subscribe("teslamate/cars/+/display_name")
+
+    def controller_on_message(self, client, userdata, msg):
+      LOGGER.debug('MQTT message {}:{}'.format(msg.topic, msg.payload))
+      topic = msg.topic.split('/')
+      vehicleNumber = topic[2]
+      statusItem = topic[3]
+      payload = msg.payload.decode('utf-8')
+      LOGGER.debug('Split: vehicleNumber {}, statusItem {}, payload {}'.format(vehicleNumber, statusItem, payload))
+      
+      # find the vehicle node
+      nodeAddress = str(vehicleNumber)
+      if bool(self.nodes.get(nodeAddress)): 
+        LOGGER.debug('Vehicle was found in list')
+      else:
+        LOGGER.debug('Vehicle was NOT found in list')
+        # not found, so if this is the display_name variable, add the node
+        if statusItem == 'display_name':
+          LOGGER.info('Adding ndoe with address {} and name {}'.format(nodeAddress, payload))
+          self.addNode(VehicleNode(self, self.address, nodeAddress, payload, self.MQTT_HOST, vehicleNumber))
+        fi
+      fi
+
+    def __init__(self, polyglot):
+        super(Controller, self).__init__(polyglot)
+        self.name = 'TeslaMate Controller'
+        self.poly.onConfig(self.process_config)
+        self.client = mqtt.Client()
+        self.client.on_connect = self.controller_on_connect
+        self.client.on_message = self.controller_on_message
+        LOGGER.debug('Done with __init__')
+
+    def start(self):
+        # This grabs the server.json data and checks profile_version is up to date
+        serverdata = self.poly.get_server_data()
+        LOGGER.info('Started TeslaMate NodeServer {}'.format(serverdata['version']))
+        self.heartbeat(0)
+        self.check_params()
+        #self.discover()
+        self.poly.add_custom_config_docs("")
+        self.poly.installprofile()
+
+    def shortPoll(self):
+        LOGGER.debug('shortPoll - done checking TeslaMate')
+
+    def longPoll(self):
+        LOGGER.debug('longPoll')
+        self.heartbeat()
+
+    def query(self,command=None):
+        self.check_params()
+        for node in self.nodes:
+            self.nodes[node].reportDrivers()
+
+    def discover(self, *args, **kwargs):
+        LOGGER.debug('Controller - discover')
+        self.client.connect(self.MQTT_HOST, 1883, 60)
+        self.client.loop_start()
+        # give it 10 seconds to get all the vehicles
+        LOGGER.debug("Discover - sleeping for 10 seconds for discovery")
+        time.sleep(10)
+        LOGGER.debug("Discover - slept for 10 seconds for discovery - stopping discovery")
+        self.client.loop_stop()
+        return
+          
+    def delete(self):
+        LOGGER.info('Oh God I\'m being deleted. Nooooooooooooooooooooooooooooooooooooooooo.')
+
+    def stop(self):
+        LOGGER.debug('NodeServer stopped.')
+
+    def process_config(self, config):
+        # this seems to get called twice for every change, why?
+        # What does config represent?
+        LOGGER.info("process_config: Enter config={}".format(config));
+        LOGGER.info("process_config: Exit");
+
+    def heartbeat(self,init=False):
+        LOGGER.debug('heartbeat: init={}'.format(init))
+        if init is not False:
+            self.hb = init
+        LOGGER.debug('heartbeat: hb={}'.format(self.hb))
+        if self.hb == 0:
+            self.reportCmd("DON",2)
+            self.hb = 1
+        else:
+            self.reportCmd("DOF",2)
+            self.hb = 0
+
+    def check_params(self):
+        default_api_key = "ABCDEFG"
+        self.removeNoticesAll()
+
+        if 'DebugLevel' in self.polyConfig['customParams']:
+            LOGGER.debug('DebugLevel found in customParams')
+            self.DebugLevel = self.polyConfig['customParams']['DebugLevel']
+            LOGGER.debug('check_params: DebugLevel is: {}'.format(self.DebugLevel))
+            if self.DebugLevel == '':
+                LOGGER.debug('check_params: DebugLevel is empty')
+                self.DebugLevel = int(logging.INFO)
+                LOGGER.debug('check_params: DebugLevel is defined in customParams, but is blank - please update it.  Using {}'.format(self.DebugLevel))
+                self.addNotice('Set \'DebugLevel\' and then restart')
+                st = False
+        else:
+            LOGGER.debug('check_params: DebugLevel does not exist self.polyCconfig: {}'.format(self.polyConfig))
+            self.DebugLevel = int(logging.INFO)
+            LOGGER.debug('check_params: DebugLevel not defined in customParams, setting to {}'.format(self.DebugLevel))
+            st = False
+
+        # convert string to int
+        self.DebugLevel = int(self.DebugLevel)
+
+        # Set the debug level based on parameter
+        LOGGER.setLevel(self.DebugLevel)
+        LOGGER.warning('Setting debug level to {}'.format(self.DebugLevel))
+        self.setDriver('GV0', self.DebugLevel)
+        LOGGER.warning('Done setting debug level to {}'.format(self.DebugLevel))
+
+        if 'MQTT_HOST' in self.polyConfig['customParams']:
+            LOGGER.debug('MQTT_HOST found in customParams')
+            self.MQTT_HOST = self.polyConfig['customParams']['MQTT_HOST']
+            LOGGER.debug('check_params: MQTT_HOST is: {}'.format(self.MQTT_HOST))
+            if self.MQTT_HOST == '' or self.MQTT_HOST == default_api_key:
+                LOGGER.debug('check_params: MQTT_HOST is empty')
+                self.MQTT_HOST = default_api_key
+                LOGGER.debug('check_params: MQTT_HOST is defined in customParams, but is blank - please update it.  Using {}'.format(self.MQTT_HOST))
+                self.addNotice('Set \'MQTT_HOST\' and then restart')
+                st = False
+        else:
+            LOGGER.debug('check_params: MQTT_HOST does not exist self.polyCconfig: {}'.format(self.polyConfig))
+            self.MQTT_HOST = default_api_key
+            LOGGER.debug('check_params: MQTT_HOST not defined in customParams, please update it.  Using {}'.format(self.MQTT_HOST))
+            self.addNotice('Set \'MQTT_HOST\' and then restart')
+            st = False
+
+        LOGGER.debug('Done checking: self.MQTT_HOST  = {}'.format(self.MQTT_HOST))
+        LOGGER.debug('Done checking: self.DebugLevel = {}'.format(self.DebugLevel))
+
+        # Make sure they are in the params
+        self.addCustomParam({'DebugLevel': self.DebugLevel, 'MQTT_HOST': self.MQTT_HOST})
+
+    def remove_notice_test(self,command):
+        LOGGER.info('remove_notice_test: notices={}'.format(self.poly.config['notices']))
+        # Remove all existing notices
+        self.removeNotice('test')
+
+    def remove_notices_all(self,command):
+        LOGGER.info('remove_notices_all: notices={}'.format(self.poly.config['notices']))
+        # Remove all existing notices
+        self.removeNoticesAll()
+
+    def update_profile(self,command):
+        LOGGER.info('update_profile:')
+        st = self.poly.installprofile()
+        return st
+
+    def set_debug_level(self,command):
+        self.DebugLevel = int(command['value'])
+        LOGGER.warning("New debug level: {}".format(self.DebugLevel))
+        self.setDriver('GV0', self.DebugLevel)
+        LOGGER.setLevel(self.DebugLevel)
+
+        # Make sure they are in the params
+        self.addCustomParam({'DebugLevel': self.DebugLevel, 'MQTT_HOST': self.MQTT_HOST})
+
+    def setOn(self, command):
+       self.setDriver('ST', 1)
+
+    def setOff(self, command):
+       self.setDriver('ST', 0)
+
+    id = 'controller'
+    commands = {
+        'QUERY': query,
+        'DISCOVER': discover,
+        'UPDATE_PROFILE': update_profile,
+        'REMOVE_NOTICES_ALL': remove_notices_all,
+        'REMOVE_NOTICE_TEST': remove_notice_test,
+        'SET_DEBUG_LEVEL': set_debug_level,
+    }
+    drivers = [{'driver': 'ST',  'value': 0, 'uom': 2},
+               {'driver': 'GV0', 'value': 0, 'uom': 25}
+    ]
+
+class VehicleNode(polyinterface.Node):
+  def __init__(self, controller, primary, address, name, mqttHost, vehicleNumber):
+    super(VehicleNode, self).__init__(controller, primary, address, name)
+    self.vehicleNumber = vehicleNumber
+    self.MQTT_HOST = mqttHost
+    self.client = mqtt.Client()
+    self.client.on_connect = self.vehicle_on_connect
+    self.client.on_message = self.vehicle_on_message
+
+  def vehicle_on_connect(self, client, userdata, flags, rc):
+      LOGGER.debug('Vehicle {} connected with result code '.format(self.vehicleNumber, str(rc)))
+      subscribeTopic = "teslamate/cars/" + self.vehicleNumber + "/#"
+      LOGGER.debug("Vehicle # {} subscribing to topic {}".format(self.vehicleNumber, subscribeTopic))
+      client.subscribe(subscribeTopic)
+
+  def vehicle_on_message(self, client, userdata, msg):
+      LOGGER.debug('Vehicle MQTT message {}:{}'.format(msg.topic, msg.payload))
+      topic = msg.topic.split('/')
+      vehicleNumber = topic[2]
+      statusItem = topic[3]
+      payload = msg.payload.decode('utf-8')
+
+      d = {"state"                  : self.vehicle_state,
+           "odometer"               : self.vehicle_odometer,
+           "locked"                 : self.vehicle_locked,
+           "est_battery_range_km"   : self.vehicle_est_battery_range_km,
+           "rated_battery_range_km" : self.vehicle_rated_battery_range_km,
+           "usable_battery_level"   : self.vehicle_usable_battery_level}
+      try:
+        d[statusItem](payload)
+      except KeyError:
+        LOGGER.debug("Status item '{}' unimplemented".format(statusItem))
+
+  def start(self):
+    LOGGER.debug('Vehicle start - calling mqtt connect')
+    self.client.connect(self.MQTT_HOST, 1883, 60)
+    self.client.loop_start()
+    LOGGER.debug('Vehicle start - called  mqtt connect')
+    pass
+
+  def shortPoll(self):
+    LOGGER.debug('VehicleNode - shortPoll')
+
+  def longPoll(self):
+    LOGGER.debug('VehicleNode - longPoll')
+
+  def setOn(self, command):
+    self.setDriver('ST', 1)
+
+  def setOff(self, command):
+    self.setDriver('ST', 0)
+
+  def query(self,command=None):
+    self.reportDrivers()
+
+  def vehicle_locked(self, payload):
+    LOGGER.debug('lock status received: {}'.format(payload))
+    if payload == "true":
+      self.setDriver('GV7', 1)
+    else:
+      self.setDriver('GV7', 0)
+    LOGGER.debug('lock status updated: {}'.format(payload))
+
+  def vehicle_odometer(self, payload):
+    miles = float(payload) * MILES_PER_KM;
+    self.setDriver('GV6', round(miles, 2))
+
+  def vehicle_est_battery_range_km(self, payload):
+    miles = float(payload) * MILES_PER_KM;
+    self.setDriver('GV3', round(miles, 2))
+
+  def vehicle_rated_battery_range_km(self, payload):
+    miles = float(payload) * MILES_PER_KM;
+    self.setDriver('GV5', round(miles, 2))
+
+  def vehicle_usable_battery_level(self, payload):
+    self.setDriver('GV2', payload)
+
+  def vehicle_state(self, payload):
+    LOGGER.debug('vehicleNumber {} - state: {}'.format(self.vehicleNumber, payload))
+    sleepState = {'asleep': 0,
+                  'idle'  : 1,
+                  'driving': 2,
+                  'charging' : 3}
+    LOGGER.debug("Setting vehicle_state to {}".format(sleepState[payload]))
+    self.setDriver('GV1', sleepState[payload])
+    LOGGER.debug("Set     vehicle_state to {}".format(sleepState[payload]))
+                
+  # hint = [1,2,3,4]
+  drivers = [{'driver': 'ST',  'value': 0, 'uom': 2},
+             {'driver': 'GV0', 'value': 0, 'uom': 25},
+             {'driver': 'GV1', 'value': 0, 'uom': 25},
+             {'driver': 'GV2', 'value': 0, 'uom': 51},
+             {'driver': 'GV3', 'value': 0, 'uom': 116},
+             {'driver': 'GV4', 'value': 0, 'uom': 51},
+             {'driver': 'GV5', 'value': 0, 'uom': 116},
+             {'driver': 'GV6', 'value': 0, 'uom': 116},
+             {'driver': 'GV7', 'value': 0, 'uom': 25},
+  ]
+  id = 'vehicle'
+  commands = {
+      'DON': setOn, 'DOF': setOff
+  }
+
+if __name__ == "__main__":
+    try:
+        polyglot = polyinterface.Interface('TeslaMateNS')
+        """
+        Instantiates the Interface to Polyglot.
+        The name doesn't really matter unless you are starting it from the
+        command line then you need a line Template=N
+        where N is the slot number.
+        """
+        polyglot.start()
+        """
+        Starts MQTT and connects to Polyglot.
+        """
+        control = Controller(polyglot)
+        """
+        Creates the Controller Node and passes in the Interface
+        """
+        control.runForever()
+        """
+        Sits around and does nothing forever, keeping your program running.
+        """
+    except (KeyboardInterrupt, SystemExit):
+        LOGGER.warning("Received interrupt or exit...")
+        """
+        Catch SIGTERM or Control-C and exit cleanly.
+        """
+        polyglot.stop()
+    except Exception as err:
+        LOGGER.error('Excption: {0}'.format(err), exc_info=True)
+    sys.exit(0)
